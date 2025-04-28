462ee3d6881e43d38e160f132113a70ea2c75534
figuerrero26 <figuerrero26@ucatolica.edu.co>
2025-04-28 01:48:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.include_router(user_router)
 
 @app.get("/")
 def read_root():
-    return {"message": "API funcionando correctamente 🚀-Frank uwu"}
+    return {"message": "API funcionando correctamente 🚀-Frank"}
 
 
 print("Las tablas han sido creadas correctamente en la base de datos.")
