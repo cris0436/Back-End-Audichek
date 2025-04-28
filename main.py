f77a2a353ef9ed62c70617b4c2acc4b0dd4239b4
figuerrero26 <figuerrero26@ucatolica.edu.co>
2025-04-27 23:18:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'Back-End-Audichek')))
+
+
 from database.Connections import engine, Base
 from models import *
 from models import User
