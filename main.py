ecf165e7cc21902f956f1fa6eced095cc9c414bc
cris0436 <cacabezas36@ucatolica.edu.co>
2025-04-28 01:09:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'Back-End-Audichek')))
-
-from database.Conections import Base
+from fastapi import FastAPI
+from database.connections import engine, Base
 from models import *
-from models import User
+from routers.user_routes import router as user_router  # Importamos el router
 
+app = FastAPI()
+
+# Crear tablas automáticamente
 Base.metadata.create_all(bind=engine)
+
+# Incluir routers
+app.include_router(user_router)
+
+@app.get("/")
+def read_root():
+    return {"message": "API funcionando correctamente 🚀"}
 
 print("Las tablas han sido creadas correctamente en la base de datos.")
